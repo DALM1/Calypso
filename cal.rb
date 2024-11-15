7779fa4113852872872aed947cb4139c997e97f4
Dimitri Almon <dimitri.almon@gmail.com>
2024-11-15 09:40:56 +0100

    re try rem

--- cal.rb
+++ cal.rb
@@ -1,6 +1,6 @@
 require 'socket'
 
-server_ip = ENV['SERVER_IP'] || '0.0.0.0'
+server_ip = ENV['SERVER_IP'] || '195.35.1.108'
 server_port = ENV['SERVER_PORT'] ? ENV['SERVER_PORT'].to_i : 3630
 
 client = TCPSocket.new(server_ip, server_port)
