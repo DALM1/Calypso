b16f10b9067030727a60773824ed25d3f0ffc459
Dimitri Almon <dimitri.almon@gmail.com>
2024-11-15 08:16:58 +0100

    re try rem

--- models/ music_manager.rb
+++ models/ music_manager.rb
@@ -1,9 +1,9 @@
 class MusicManager
   def self.play(chat_room)
-    chat_room.broadcast_message("Playing music...")
+    chat_room.broadcast_message("Playing music in the room!", "MusicManager")
   end
 
   def self.stop(chat_room)
-    chat_room.broadcast_message("Music stopped.")
+    chat_room.broadcast_message("Music stopped.", "MusicManager")
   end
 end
